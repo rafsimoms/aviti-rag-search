a97ff3864d0f03d549a0e401f3472e0f9ed04e2a
rafsimoms <dm1triysergeev@mail.ru>
2026-07-18 18:13:50 +0300

    modified evaluate and make some experiments

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -6,8 +6,10 @@ import time
 from ranx import Qrels, Run, evaluate as ranx_evaluate
 import random
 import json
-from retrievers import BM25Retriever, DenseRetriever, HybridRetriever, RerankRetriever
+from datetime import datetime
+from retrievers import BM25Retriever, DenseRetriever, HybridRetriever, RerankRetriever, RandomRetriever
 from tqdm.auto import tqdm
+import argparse
 
 def load_config(path):
     return yaml.safe_load(Path(path).read_text())
@@ -22,6 +24,7 @@ def get_data(path):
 
 def evaluate_retriever(retriever, path):
     data = get_data(path)
+    retriever.search(data[0]["query_text"])
     qrels = {}
     run = {}
     latencies = []
@@ -35,19 +38,58 @@ def evaluate_retriever(retriever, path):
     run_ranx = Run(run)
     map_at_10 = ranx_evaluate(qrels_ranx, run_ranx, "map@10")
     avg_latency_ms = 1000 * sum(latencies) / len(latencies)
-    print(f"map10: {map_at_10}, latency: {avg_latency_ms}")
-    return map_at_10
-    
-if __name__ == "__main__":
+    return (map_at_10, avg_latency_ms)
+
+def build_retriver(name, config):
+    if name == 'random':
+        df = pd.read_parquet('../' + config['paths']['processed_acricles'])['erticle_id'].astype(str).tolist()
+        r = RandomRetriever(article_ids=df, top_k=config['search']['top_k'])
+        return r
+    if name == 'bm25':
+        r = BM25Retriever(config=config)
+        r.load('../' + config['paths']['bm25_index'])
+        return r
+    if name == 'dense':
+        r = DenseRetriever(config=config)
+        r.load('../' + config['paths']['dense_index'])
+        return r
+    if name == 'hybrid':
+        r = HybridRetriever(config, [build_retriver('bm25', config), build_retriver('dense', config)])
+        return r
+    if name == 'reranker':
+        r = RerankRetriever(config, build_retriver('hybrid', config))
+        return r
+    raise ValueError(f"Такого ретривера не существует: {name}")
+
+def log_experiment(config, retriever_name, map_at_10, avg_latency_ms):
+    block = '\n'.join(['=' * 60,
+                       f"time: {datetime.now().isoformat(timespec='seconds')}", 
+                       f"retriever_name: {retriever_name}",
+                       f'map@10: {map_at_10}',
+                       f"latency: {avg_latency_ms}",
+                       f"chunk_size: {config['chunking']['chunk_size']}",
+                       f"overlap: {config['chunking']['chunk_overlap']}",
+                       f"search cfg: ",
+                       yaml.dump(config['search'], allow_unicode=True, sort_keys=False).rstrip(),
+                       '=' * 60
+                       ])
+    with open('../' + config['paths']['logs'], 'a', encoding='UTF-8') as f:
+        f.write(block + '\n')
+
+
+def main():
     config = load_config("../config.yaml")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--retriever', required=True, choices=['random', 'bm25', 'dense', 'hybrid', 'reranker'])
+    args = parser.parse_args()
+    retriever = build_retriver(args.retriever, config=config)
+    map_at_10, avg_latency_ms = evaluate_retriever(retriever=retriever, path='../' + config['paths']['calibration'])
+    print(f"map10: {map_at_10}, latency: {avg_latency_ms}")
+    log_experiment(config=config, retriever_name=args.retriever, map_at_10=map_at_10, avg_latency_ms=avg_latency_ms)
 
-    bm25 = BM25Retriever(config)
-    bm25.load("../" + config["paths"]["bm25_index"])
 
-    dense = DenseRetriever(config)
-    dense.load(Path("../" + config["paths"]["dense_index"]))
 
-    hybrid = HybridRetriever(config, [bm25, dense])
-    reranked = RerankRetriever(config, hybrid)
+if __name__ == "__main__":
+    main()
 
-    evaluate_retriever(reranked, "../" + config["paths"]["calibration"])
+
